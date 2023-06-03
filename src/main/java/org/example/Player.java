1b7fa9f55f2b2e967c2b1d6aae0741fff30f34ce
botsheloramela <ramelabotshelo12@gmail.com>
2023-06-03 16:22:49 +0200

    feat: New Player class

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,8 @@
+package org.example;
+
+/**
+ * Represents a player in the game.
+ * It has properties such as name and points.
+ * It provides methods to get the player's move and update their points.*/
+public class Player {
+}
