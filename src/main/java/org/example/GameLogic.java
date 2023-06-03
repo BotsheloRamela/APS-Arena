e05805f08d79fbac73c4050b05ad32870b843432
botsheloramela <ramelabotshelo12@gmail.com>
2023-06-03 17:18:53 +0200

    feat(logic): New determineWinner() method

--- src/main/java/org/example/GameLogic.java
+++ src/main/java/org/example/GameLogic.java
@@ -4,4 +4,16 @@ package org.example;
 * Contains the game rules and logic.
 * It determines the winner based on the moves chosen by the players.*/
 public class GameLogic {
+
+    public String determineWinner(Moves playerMove, Moves cpuMove) {
+        if (playerMove == cpuMove) {
+            return "TIE";
+        } else if (playerMove.equals(Moves.ROCK) && cpuMove.equals(Moves.PAPER) ||
+                    playerMove.equals(Moves.PAPER) && cpuMove.equals(Moves.SCISSORS) ||
+                    playerMove.equals(Moves.SCISSORS) && cpuMove.equals(Moves.ROCK)) {
+            return "LOSS";
+        } else {
+            return "WIN";
+        }
+    }
 }
