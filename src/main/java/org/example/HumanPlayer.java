cf80482cd04b9aed8eec137bc237460b51e712e5
botsheloramela <ramelabotshelo12@gmail.com>
2023-06-03 16:24:37 +0200

    feat: New HumanPlayer class

---
+++ src/main/java/org/example/HumanPlayer.java
@@ -0,0 +1,7 @@
+package org.example;
+
+/**
+ *  Extends the Player class and represents a human player in multiplayer mode.
+ *  It can handle input from the human player to get their move.*/
+public class HumanPlayer {
+}
