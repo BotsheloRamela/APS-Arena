682c9ad76eb473772a1c108a76e3e4d7f6e9d19d
botsheloramela <ramelabotshelo12@gmail.com>
2023-06-03 17:20:21 +0200

    docs(logic): New determineWinner() docs

--- src/main/java/org/example/GameLogic.java
+++ src/main/java/org/example/GameLogic.java
@@ -5,6 +5,13 @@ package org.example;
 * It determines the winner based on the moves chosen by the players.*/
 public class GameLogic {
 
+    /**
+     * Determines the winner of the game based on the moves played by the player and the CPU.
+     *
+     * @param playerMove The move played by the player.
+     * @param cpuMove    The move played by the CPU.
+     * @return A string indicating the result of the game: "WIN" if the player wins, "LOSS" if the player loses, or "TIE" if it's a tie.
+     */
     public String determineWinner(Moves playerMove, Moves cpuMove) {
         if (playerMove == cpuMove) {
             return "TIE";
