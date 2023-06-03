1d360532cfc698865c59c85e34cd4a396e9122a4
botsheloramela <ramelabotshelo12@gmail.com>
2023-06-03 17:04:03 +0200

    feat(cpu): New generateCPUMove() method

--- src/main/java/org/example/ComputerPlayer.java
+++ src/main/java/org/example/ComputerPlayer.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.util.Random;
+
 /*
 * Extends the Player class and represents the computer player in single-player mode.
 * It implements a strategy to generate a random move for the computer.*/
 public class ComputerPlayer {
+    private int cpuPoints = 0;
+
+    private Moves generateCPUMove() {
+        Moves[] moves = Moves.values();
+        Random random = new Random();
+        int index = random.nextInt(moves.length);
+        return moves[index];
+    }
 }
