6577c200af4299f234342330809893baf96e8003
botsheloramela <ramelabotshelo12@gmail.com>
2023-06-03 16:23:42 +0200

    feat: New ComputerPlayer class

---
+++ src/main/java/org/example/ComputerPlayer.java
@@ -0,0 +1,7 @@
+package org.example;
+
+/*
+* Extends the Player class and represents the computer player in single-player mode.
+* It implements a strategy to generate a random move for the computer.*/
+public class ComputerPlayer {
+}
